b9131095e65f0fe9b8d1e84a1d22487925e55cfd
midnight-vivian <midnight.vivian@gmail.com>
2017-12-22 21:45:52 +0800

    add 111. Minimum Depth of Binary Tree

---
+++ 111. Minimum Depth of Binary Tree/minimum_depth_of_binary_tree.go
@@ -0,0 +1,22 @@
+package minimum_depth_of_binary_tree
+
+import (
+	"github.com/midnight-vivian/go-data-structures/src/utils"
+)
+
+/*
+Given a binary tree, find its minimum depth.
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+*/
+
+func minDepth(root *utils.TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	lDepth := minDepth(root.Left)
+	rDepth := minDepth(root.Right)
+	if (lDepth != 0 && lDepth < rDepth) || rDepth == 0 {
+		return lDepth + 1
+	}
+	return rDepth + 1
+}
---
+++ 111. Minimum Depth of Binary Tree/minimum_depth_of_binary_tree_test.go
@@ -0,0 +1,28 @@
+package minimum_depth_of_binary_tree
+
+import (
+	"testing"
+
+	"github.com/midnight-vivian/go-data-structures/src/utils"
+)
+
+func TestMinDepth(t *testing.T) {
+	vals := []interface{}{}
+	tree := utils.BinaryTreeConstructor(vals)
+	result := 0
+	if result != minDepth(tree) {
+		t.Error("minDepth error")
+	}
+	vals = []interface{}{1}
+	tree = utils.BinaryTreeConstructor(vals)
+	result = 1
+	if result != minDepth(tree) {
+		t.Error("minDepth error")
+	}
+	vals = []interface{}{1, 2, 3, 4, nil, 5, nil, nil, nil, nil, 7}
+	tree = utils.BinaryTreeConstructor(vals)
+	result = 3
+	if result != minDepth(tree) {
+		t.Error("minDepth error")
+	}
+}
