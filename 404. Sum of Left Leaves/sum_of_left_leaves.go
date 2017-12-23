8d41b73385a9b9580362c395964df8142fefdbe5
midnight-vivian <midnight.vivian@gmail.com>
2017-12-23 14:34:43 +0800

    add 404. Sum of Left Leaves

---
+++ 404. Sum of Left Leaves/sum_of_left_leaves.go
@@ -0,0 +1,29 @@
+package sum_of_left_leaves
+
+import (
+	"github.com/midnight-vivian/go-data-structures/src/utils"
+)
+
+/*
+Find the sum of all left leaves in a given binary tree.
+
+Example:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+*/
+
+func sumOfLeftLeaves(root *utils.TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	sum := sumOfLeftLeaves(root.Right) + sumOfLeftLeaves(root.Left)
+	if root.Left != nil && root.Left.Left == nil && root.Left.Right == nil {
+		sum += root.Left.Val
+	}
+	return sum
+}
---
+++ 404. Sum of Left Leaves/sum_of_left_leaves_test.go
@@ -0,0 +1,30 @@
+package sum_of_left_leaves
+
+import (
+	"testing"
+
+	"github.com/midnight-vivian/go-data-structures/src/utils"
+)
+
+func TestSumOfLeftLeaves(t *testing.T) {
+	vals := []interface{}{}
+	tree := utils.BinaryTreeConstructor(vals)
+	result := 0
+	if result != sumOfLeftLeaves(tree) {
+		t.Error("sumOfLeftLeaves error")
+	}
+
+	vals = []interface{}{1}
+	tree = utils.BinaryTreeConstructor(vals)
+	result = 0
+	if result != sumOfLeftLeaves(tree) {
+		t.Error("sumOfLeftLeaves error")
+	}
+
+	vals = []interface{}{1, 2, 3, 4, nil, 5, 6, nil, nil, nil, 7, 8, nil}
+	tree = utils.BinaryTreeConstructor(vals)
+	result = 12
+	if result != sumOfLeftLeaves(tree) {
+		t.Error("sumOfLeftLeaves error")
+	}
+}
