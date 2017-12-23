f0058b9ee8dc8c2b6eb955b8c851a615d6c3ed80
midnight-vivian <midnight.vivian@gmail.com>
2017-12-23 22:58:50 +0800

    add 101. Symmetric Tree

---
+++ 101. Symmetric Tree/symmetric_tree.go
@@ -0,0 +1,69 @@
+package symmetric_tree
+
+import (
+	"github.com/midnight-vivian/go-data-structures/src/data-structures/stack"
+	"github.com/midnight-vivian/go-data-structures/src/utils"
+)
+
+/*
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+*/
+
+func isSymmetricRecursion(root *utils.TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return isSymmetricRecursionInner(root.Left, root.Right)
+}
+
+func isSymmetricRecursionInner(n1, n2 *utils.TreeNode) bool {
+	if n1 == nil && n2 == nil {
+		return true
+	}
+	if n1 == nil || n2 == nil || n1.Val != n2.Val {
+		return false
+	}
+	if isSymmetricRecursionInner(n1.Left, n2.Right) && isSymmetricRecursionInner(n1.Right, n2.Left) {
+		return true
+	}
+	return false
+}
+
+func isSymmetricIteration(root *utils.TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	s := stack.NewStackSlice()
+	s.Push(root.Left)
+	s.Push(root.Right)
+	for !s.Empty() {
+		n1, n2 := s.Pop().(*utils.TreeNode), s.Pop().(*utils.TreeNode)
+		if n1 == nil && n2 == nil {
+			continue
+		}
+		if n1 == nil || n2 == nil || n1.Val != n2.Val {
+			return false
+		}
+		s.Push(n1.Left)
+		s.Push(n2.Right)
+		s.Push(n1.Right)
+		s.Push(n2.Left)
+	}
+	return true
+}
---
+++ 101. Symmetric Tree/symmetric_tree_test.go
@@ -0,0 +1,70 @@
+package symmetric_tree
+
+import (
+	"testing"
+
+	"github.com/midnight-vivian/go-data-structures/src/utils"
+)
+
+func TestIsSymmetric(t *testing.T) {
+	vals := []interface{}{}
+	tree := utils.BinaryTreeConstructor(vals)
+	if !isSymmetricRecursion(tree) {
+		t.Error("isSymmetricRecursion error")
+	}
+
+	vals = []interface{}{1}
+	tree = utils.BinaryTreeConstructor(vals)
+	if !isSymmetricRecursion(tree) {
+		t.Error("isSymmetricRecursion error")
+	}
+
+	vals = []interface{}{1, 2, 2, 4, 3, 3, 4}
+	tree = utils.BinaryTreeConstructor(vals)
+	if !isSymmetricRecursion(tree) {
+		t.Error("isSymmetricRecursion error")
+	}
+
+	vals = []interface{}{1, 2, 3, 4, 3, 3, 4}
+	tree = utils.BinaryTreeConstructor(vals)
+	if isSymmetricRecursion(tree) {
+		t.Error("isSymmetricRecursion error")
+	}
+
+	vals = []interface{}{1, 2, 2, 4, 3, nil, 4}
+	tree = utils.BinaryTreeConstructor(vals)
+	tree = utils.BinaryTreeConstructor(vals)
+	if isSymmetricRecursion(tree) {
+		t.Error("isSymmetricRecursion error")
+	}
+
+	vals = []interface{}{}
+	tree = utils.BinaryTreeConstructor(vals)
+	if !isSymmetricIteration(tree) {
+		t.Error("isSymmetricIteration error")
+	}
+
+	vals = []interface{}{1}
+	tree = utils.BinaryTreeConstructor(vals)
+	if !isSymmetricIteration(tree) {
+		t.Error("isSymmetricIteration error")
+	}
+
+	vals = []interface{}{1, 2, 2, 4, 3, 3, 4}
+	tree = utils.BinaryTreeConstructor(vals)
+	if !isSymmetricIteration(tree) {
+		t.Error("isSymmetricIteration error")
+	}
+
+	vals = []interface{}{1, 2, 3, 4, 3, 3, 4}
+	tree = utils.BinaryTreeConstructor(vals)
+	if isSymmetricIteration(tree) {
+		t.Error("isSymmetricIteration error")
+	}
+
+	vals = []interface{}{1, 2, 2, 4, 3, nil, 4}
+	tree = utils.BinaryTreeConstructor(vals)
+	if isSymmetricIteration(tree) {
+		t.Error("isSymmetricIteration error")
+	}
+}
